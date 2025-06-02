55368eae28f71f5156c2c784fad7c35d0f041acf
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-06-02 21:46:41 +0200

    added Group Entity

---
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -0,0 +1,35 @@
+package com.kodilla.ecommercee.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "product_group")
+public class Group {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    //@OneToMany(orphanRemoval = true)
+    //@JoinColumn(name = "group_id")
+    //private List<Product> products = new ArrayList<>();
+}
