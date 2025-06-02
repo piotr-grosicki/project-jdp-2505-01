eae24a2707e2d40b57ab60a327486aae03dc6561
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-06-02 23:44:50 +0200

    added empty CartController

---
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -0,0 +1,38 @@
+package com.kodilla.ecommercee.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/carts")
+public class CartController {
+
+    @PostMapping
+    public ResponseEntity<Long> createEmptyCart() {
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{cartId}")
+    public ResponseEntity<Object> getCartContents(@PathVariable Long cartId) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{cartId}/products/{productId}")
+    public ResponseEntity<Object> addProductToCart(
+            @PathVariable Long cartId, @PathVariable Long productId
+            //@RequestParam(defaultValue = "1") int quantity
+    ) {
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{cartId}/products/{productId}")
+    public ResponseEntity<Void> removeProductFromCart(
+            @PathVariable Long cartId, @PathVariable Long productId) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{cartId}/orders")
+    public ResponseEntity<Long> convertCartToOrder(@PathVariable Long cartId) {
+        return ResponseEntity.ok().build();
+    }
+}
