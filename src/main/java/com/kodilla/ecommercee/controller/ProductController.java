cacdde0fcd23a4234e84d7e629301fd64d531cd3
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-06-03 18:15:09 +0200

    added empty ProductController

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -1,31 +1,31 @@
 package com.kodilla.ecommercee.controller;
 
-import com.kodilla.ecommercee.domain.Product;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
 
     @GetMapping
-    public ResponseEntity<List<Product>> getAllProducts() {
-        return ResponseEntity.ok(List.of());
+    public ResponseEntity<Void> getAllProducts() {
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{productId}")
-    public ResponseEntity<Product> getProduct(@PathVariable Long productId) {
+    public ResponseEntity<Long> getProduct(@PathVariable Long productId) {
         return ResponseEntity.notFound().build();
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+    public ResponseEntity<Object> createProduct(@RequestBody Object product) {
         return ResponseEntity.ok(product);
     }
 
     @PutMapping("/{productId}")
-    public ResponseEntity<Product> updateProduct(@PathVariable Long productId, @RequestBody Product updatedProduct) {
+    public ResponseEntity<Object> updateProduct(
+            @PathVariable Long productId, @RequestBody Object updatedProduct) {
         return ResponseEntity.ok(updatedProduct);
     }
 
