73208e3d087664e864ea8d621f209e4e10d49d11
Bartek Pokrywka <pokrywkamusic@gmail.com>
2025-06-06 15:01:24 +0200

    Added CartMapper

--- src/main/java/com/kodilla/ecommercee/mapper/CartMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/CartMapper.java
@@ -1,7 +1,25 @@
 package com.kodilla.ecommercee.mapper;
 
+import com.kodilla.ecommercee.domain.Cart;
+import com.kodilla.ecommercee.domain.CartStatusEnum;
+import com.kodilla.ecommercee.dto.CartDTO;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class CartMapper {
+
+    public CartDTO mapToCartDto(Cart cart) {
+        String status = cart.getStatus() != null ? cart.getStatus().name() : null;
+        String createdAt = cart.getCreatedAt() != null ? cart.getCreatedAt().toString() : null;
+        return new CartDTO(cart.getId(), status, createdAt);
+    }
+
+    public Cart mapToCart(CartDTO cartDto) {
+        return Cart.builder()
+                .status(cartDto.status() != null ? CartStatusEnum.valueOf(cartDto.status()) : null)
+                .createdAt(cartDto.createdAt() != null ? LocalDateTime.parse(cartDto.createdAt()) : null)
+                .build();
+    }
 }
