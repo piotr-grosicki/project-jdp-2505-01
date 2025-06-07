b3259f68b5f16def4922636f6c9d7502ffc6e529
Bartek Pokrywka <pokrywkamusic@gmail.com>
2025-06-07 20:17:34 +0200

    Added ProductService

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -1,31 +1,35 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.dto.ProductDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
 
     @GetMapping
-    public ResponseEntity<Void> getAllProducts() {
+    public ResponseEntity<List<ProductDTO>> getAllProducts() {
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{productId}")
-    public ResponseEntity<Void> getProduct(@PathVariable Long productId) {
+    public ResponseEntity<ProductDTO> getProduct(@PathVariable Long productId) {
         return ResponseEntity.ok().build();
     }
 
     @PostMapping
-    public ResponseEntity<String> createProduct(@RequestBody String product) {
-        return ResponseEntity.ok("product" + product);
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO dto) {
+        return ResponseEntity.ok().build();
     }
 
     @PutMapping("/{productId}")
-    public ResponseEntity<String> updateProduct(
-            @PathVariable Long productId, @RequestBody String updatedProduct) {
-        return ResponseEntity.ok("updatedProduct"+ updatedProduct);
+    public ResponseEntity<ProductDTO> updateProduct(
+            @PathVariable Long productId, @RequestBody ProductDTO dto) {
+        return ResponseEntity.ok().build();
+
     }
 
     @DeleteMapping("/{productId}")
---
+++ src/main/java/com/kodilla/ecommercee/service/ProductService.java
@@ -0,0 +1,16 @@
+package com.kodilla.ecommercee.service;
+
+import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.dto.ProductDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    public List<ProductDTO> getAllProducts() {
+        return Collections.emptyList();
+    }
+}
