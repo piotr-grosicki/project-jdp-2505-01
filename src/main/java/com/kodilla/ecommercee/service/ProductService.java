5860a474ee328313759d065ad1c1da692dc71a4e
Bartek Pokrywka <pokrywkamusic@gmail.com>
2025-06-08 01:42:17 +0200

    added new ProductRepositry method override findAll

--- src/main/java/com/kodilla/ecommercee/service/ProductService.java
+++ src/main/java/com/kodilla/ecommercee/service/ProductService.java
@@ -2,15 +2,21 @@ package com.kodilla.ecommercee.service;
 
 import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.dto.ProductDTO;
+import com.kodilla.ecommercee.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService {
 
-    public List<ProductDTO> getAllProducts() {
-        return Collections.emptyList();
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
     }
 }
