e9f4494fa8e71773cc0d6ae7051600dfdfb9aaf2
Bartek Pokrywka <pokrywkamusic@gmail.com>
2025-06-07 14:37:57 +0200

    Added CartNotFoundException

--- src/main/java/com/kodilla/ecommercee/service/CartService.java
+++ src/main/java/com/kodilla/ecommercee/service/CartService.java
@@ -2,6 +2,7 @@ package com.kodilla.ecommercee.service;
 
 import com.kodilla.ecommercee.domain.Cart;
 import com.kodilla.ecommercee.domain.CartStatusEnum;
+import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.repository.CartRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -27,9 +28,11 @@ public class CartService {
         return "Cart Content: " + cartId;
     }
 
-    public String addProductToCart(Long cartId, Long productId) {
-        // Logic to add a product to the cart
-        return "Product added to cart: " + productId;
+    public Cart addProductToCart(Cart cart, Product product) {
+        Cart existingCart = cartRepository.findById(cart.getId())
+                .orElseThrow(() -> new RuntimeException("Cart not found"));
+        existingCart.getProducts().add(product);
+        return cartRepository.save(existingCart);
     }
 
     public void removeProductFromCart(Long cartId, Long productId) {
