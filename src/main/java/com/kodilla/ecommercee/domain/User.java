9a514abedd1e9f7b4aa5db9441345252e52f3471
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-06-11 21:39:17 +0200

    Added UserConfiguration

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -18,18 +18,17 @@ public class User {
     @GeneratedValue
     private Long id;
 
-    @Setter
+
     @Column(name = "first_name")
     private String firstName;
 
-    @Setter
+
     @Column(name = "last_name")
     private String lastName;
 
     @Column(name = "password_hash")
     private String passwordHash;
 
-    @Setter
     @Column(name = "email")
     private String email;
 
