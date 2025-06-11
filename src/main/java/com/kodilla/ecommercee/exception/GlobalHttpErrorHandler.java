c884136bab60884717ac1c07890f9cfd83b69d94
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-06-11 22:12:38 +0200

    UserController

--- src/main/java/com/kodilla/ecommercee/exception/GlobalHttpErrorHandler.java
+++ src/main/java/com/kodilla/ecommercee/exception/GlobalHttpErrorHandler.java
@@ -10,11 +10,13 @@ public class GlobalHttpErrorHandler {
 
     @ExceptionHandler(UserNotFoundByIdException.class)
     public ResponseEntity<String> handleUserNotFoundById(UserNotFoundByIdException ex) {
-        return new ResponseEntity<>("User with provided id doesn't exist.", HttpStatus.NOT_FOUND);
-    }
-    @ExceptionHandler(UserNotFoundByMailException.class)
-    public ResponseEntity<String> handleUserNotFoundByMail(Exception ex) {
-        return new ResponseEntity<>("User with provided email doesn't exist.", HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>("User with provided id:" + ex.getMessage()
+                + " doesn't exist", HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(UserNotFoundByMailException.class)
+    public ResponseEntity<String> handleUserNotFoundByMail(Exception ex) {
+        return new ResponseEntity<>("User with provided email:" + ex.getMessage()
+                + " doesn't exist", HttpStatus.NOT_FOUND);
+    }
 }
